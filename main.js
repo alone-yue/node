fbb7dd2ef0e0bfa091529dcc366ab7be73c8674e
zhangyue.alone <zhangyue.alone@bytedance.com>
2022-04-14 23:22:11 +0800

    node

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ var app = express();
 
 //调用java代码
 var exec = require('child_process').exec;
-TextFilePath = "/Users/bytedance/IdeaProjects/Compiler/src/com/company/input/txt";
-ProductionPath = "/Users/bytedance/IdeaProjects/Compiler/src/com/company/input/production";
-tokenPath = "/Users/bytedance/IdeaProjects/Compiler/src/com/company/output/token";
-treePath = "/Users/bytedance/IdeaProjects/Compiler/src/com/company/output/tree";
-LL1Path = "/Users/bytedance/IdeaProjects/Compiler/src/com/company/output/LL1";
-var cmd = "java -jar Compiler.jar" + " "+ TextFilePath+ " "+ ProductionPath+ " "+ tokenPath+ " "+ treePath+ " "+ LL1Path;
+basePath = "/Users/bytedance/Desktop/node/"
+TextFilePath = basePath + "config/text";
+ProductionPath = basePath + "config/production";
+tokenPath = basePath + "config/token";
+treePath = basePath + "config/tree";
+LL1Path = basePath + "config/LL1";
+var cmd = "java -jar config/Compiler.jar" + " "+ TextFilePath+ " "+ ProductionPath+ " "+ tokenPath+ " "+ treePath+ " "+ LL1Path;
 function execute(){
   exec(cmd, function(err, stdout, stderr) {
     // 获取命令执行的输出
@@ -23,41 +24,28 @@ function execute(){
 //中间件
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use('/',express.static('./'))
+app.use('/',express.static('./front_end'))
 
-//文件相关
-var fs = require("fs");
-function callBackWrite(err) {
-  if (err) {
-      return console.error(err);
-  }
-}
-
-//handler方法
+//handler
 app.post('/process_post' ,function (req, res) {
-  //req获取数据写入文件---两个输入框
-  // fs.writeFileSync(TextFilePath, req.body.textData);
-  // fs.writeFileSync(ProductionPath, req.body.productionData);
-  fs.writeFileSync('./textData', req.body.textData);
-  fs.writeFileSync('./productionData', req.body.productionData);
+  //req->code
+  var fs = require("fs");
+  fs.writeFileSync(TextFilePath, req.body.textData);
 
-  console.log(req.body)
   //调用java逻辑，生成输出到文件中
-  // execute();
+  execute();
 
   //读取java生成的文件
-  // var token = fs.readFileSync(tokenPath, 'utf8');
-  // var tree = fs.readFileSync(treePath, 'utf8');
-  // var LL1 = fs.readFileSync(LL1Path, 'utf8');
+  var token = fs.readFileSync(tokenPath, 'utf8');
+  var tree = fs.readFileSync(treePath, 'utf8');
+  var LL1 = fs.readFileSync(LL1Path, 'utf8');
 
+  //返回
   result = {
-    textData:req.body.textData,
-    productionData : req.body.productionData,
-    token:'token',
-    tree:'tree',
-    LL1:'LL1'
+    token: token,
+    tree: tree,
+    LL1: LL1
   }
-  //把读取到的文件返回给前端---三个输出框
   res.end(JSON.stringify(result))
 })
  
